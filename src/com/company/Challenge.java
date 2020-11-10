bc9d2ebc3376a042aa3726e6f0cf95a84c0dc266
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-11-10 07:44:18 +0200

    Initial commit

---
+++ src/com/company/Challenge.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Challenge {
+    /**
+     * When overriding the equals() method in the HeavenlyBody class, we
+     * were careful to make sure that it would not return true if a HeavenlyBody
+     * was compared to a subclass of itself.
+     *
+     * We did that to demonstrate that method, but it was actually
+     * unnecessary in the HeavenlyBody class.
+     *
+     * The mini challenge is just a question: why was it unnecessary?
+     *
+     * Hint: If you are stuck, check out Lecture 97
+     *
+     * Answer:
+     *
+     *
+     **/
+
+
+
+}
